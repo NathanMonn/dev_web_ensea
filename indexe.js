780537be536d76ac12479be0309861b718872f86
NathanMonn <nathan.monnier@ensea.fr>
2024-09-20 10:47:24 +0200

    new file:   dev_web_ensea.code-workspace 	new file:   indexe.js

---
+++ indexe.js
@@ -0,0 +1,8 @@
+console.log("hello world !")
+let message = "hello world !"
+console.log(message)
+var firstname = "Nahan"
+let lastname = "MONNIER"
+message="hello "+firstname+" "+lastname+"!" 
+console.log(message)
+//console.log("hello "+firstname+" "+lastname+"!") //console.log(`hello ${firstname} ${lastname}!`)
