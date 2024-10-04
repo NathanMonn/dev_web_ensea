fa552fed792ff6fdc725ddbc9d322aaccb62a4e0
NathanMonn <nathan.monnier@ensea.fr>
2024-10-04 15:04:01 +0200

    modified:   indexe2.js

--- indexe2.js
+++ indexe2.js
@@ -5,21 +5,21 @@ let student = {
 	courses: []
   };
   
-  // Modification de l'âge et ajout de la propriété grade
-  student.age = 21;
-  student.grade = "A";
-  
-  // Ajout des éléments au tableau courses
-  student.courses.push("Math", "Physics", "Chemistry");
-  
-  // Recherche de l'index de "Physics"
-  let physicsIndex = student.courses.indexOf("Physics");
-  
-  // Création d'un nouveau tableau contenant les deux premiers éléments
-  let firstTwoCourses = student.courses.slice(0, 2);
-  
-  // Affichage des résultats
-  console.log("Objet student après modifications :", student);
-  console.log("Index de 'Physics' :", physicsIndex);
-  console.log("Nouveau tableau avec slice :", firstTwoCourses);
+// Modification de l'âge et ajout de la propriété grade
+student.age = 21;
+student.grade = "A";
+
+// Ajout des éléments au tableau courses
+student.courses.push("Math", "Physics", "Chemistry");
+
+// Recherche de l'index de "Physics"
+let physicsIndex = student.courses.indexOf("Physics");
+
+// Création d'un nouveau tableau contenant les deux premiers éléments
+let firstTwoCourses = student.courses.slice(0, 2);
+
+// Affichage des résultats
+console.log("Objet student après modifications :", student);
+console.log("Index de 'Physics' :", physicsIndex);
+console.log("Nouveau tableau avec slice :", firstTwoCourses);
   
