f310ecf629fb5ef390c8bcd7e61d3219a7061bd4
NathanMonn <nathan.monnier@ensea.fr>
2024-10-24 14:03:37 +0200

    modified:   Rest_API.js

--- Rest_API.js
+++ Rest_API.js
@@ -1,2 +1,12 @@
-console.log("test from node");
-console.log("test from node", Math.random(), new Date());
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Bienvenue sur notre API REST simple!');
+});
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
