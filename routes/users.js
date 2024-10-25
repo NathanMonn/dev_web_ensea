93e23564d0cc15fcdc04129674c0cf698359f511
NathanMonn <nathan.monnier@ensea.fr>
2024-10-25 09:33:03 +0200

    new file:   controllers/usersControllers.js 	modified:   routes/users.js 	modified:   users.db

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const { getAllUsers,createNewUser,updateUser,deleteUser } = require("../controllers/usersControllers")
 
-const db = require("../database")
 /*
 const userArray = [
 	{ id: 1, firstName: "John", lastName: "Doe", role: "admin" },
@@ -11,41 +11,11 @@ const userArray = [
 	{ id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
 ]*/
 
-router.get("/users", (req, res) => {
-    //res.json(userArray)
-	db.all('SELECT * FROM users', [], (err, rows) => {
-		if (err) {
-		  res.status(500).json({ error: err.message })
-		} else {
-		  res.json(rows)
-		}
-	})
-	
-})
+router.get("/users", getAllUsers)
 
 
 // POST : CRÉER un nouvel utilisateur, basé sur les données passées dans le corps(body) de la requête
-router.post("/users", (req, res) => {
-	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
-	const { firstName, lastName } = req.body
-
-	// récupérer l'ID du dernier utilisateur en fonction du nombre d'utilisateurs dans notre variable de tableau 'users'.
-	const lastId = users[users.length - 1].id
-	// ajouter un pour créer un utilisateur unique
-	const newId = lastId + 1
-
-	// créer le nouvel utilisateur avec les données du corps de la requête et l'ID calculé
-	const newUser = {
-		firstName,
-		lastName,
-		id: newId,
-	}
-
-	// ajouter le nouvel utilisateur à notre liste d'utilisateurs en utilisant la méthode 'push'
-	users.push(newUser)
-	// envoyer le code de statut 201 (créé) et les données du nouvel utilisateur afin de confirmer au client.
-	res.status(201).json(newUser)
-})
+router.post("/users", createNewUser)
 
 // GET : LIRE tous les utilisateurs
 /*app.get("/", (req, res) => {
@@ -62,41 +32,9 @@ router.post("/users", (req, res) => {
 })*/
 
 // GET : LIRE tous les utilisateurs
-router.put("/users/:id", (req, res) => {
-	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
-	const { firstName, lastName } = req.body
-	const id = parseInt(req.params.id)
-	// trouve son index, verifier si le userIndex est positive
-	const userIndex = users.findIndex((user) => user.id === id)
-		// utilisateur non trouvé
-		if (userIndex < 0)
-			return res.status(404).json({ msg: "utilisateur non trouvé" })
-	// si el est trouvé, nous vérifions quelles valeurs ont été envoyées
-
-	if (firstName) users[userIndex].firstName = firstName
-	if (lastName) users[userIndex].lastName = lastName
-	res.json({
-		msg: "utilisateur mis à jour",
-		user: users[userIndex],
-	})
-})
+router.put("/users/:id", updateUser)
 
 // GET : LIRE tous les utilisateurs
-router.delete("/users/:id", (req, res) => {
-	const id = parseInt(req.params.id)
-	// trouve son index, verifier si le userIndex est positive
-	const userIndex = users.findIndex((user) => user.id === id)
-
-	// utilisateur non trouvé
-	if (userIndex < 0)
-		return res.status(404).json({ msg: "utilisateur non trouvé" })
-
-	// si el est trouvé
-	users.splice(userIndex, 1)
-
-	res.json({
-		msg: "utilisateur suprimée",
-	})
-})
+router.delete("/users/:id", deleteUser)
 
 module.exports = router
